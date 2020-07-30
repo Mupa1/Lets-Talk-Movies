cb0fd125258a089aeaa822339f04d7f3c4d6152c
Mupa1 <mupasmail@gmail.com>
2020-07-30 10:08:29 +0200

    add session controller and views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  
+  end
+
+  def create
+    user = User.find_by(Username: params[:session][:Username].downcase)
+    if user
+      session[:user_id] = user.id
+      flash[:success] = 'You have successfully logged in'
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'There was something wrong with your login information'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'You have logged out'
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   resources :users, except: [:new]
 end
